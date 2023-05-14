b784920db2ad92330bbf4ba88936328143b7723b
Luxcool <Breakrun565@gmail.com>
2023-05-14 11:45:50 -0400

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Powers of 2 Tree",
+	id: "Tree Games",
+	author: "Luxcool",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
