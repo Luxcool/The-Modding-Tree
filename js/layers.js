eafe853b9c2cf4525dcd19fd425480c27f4aebd3
Luxcool <Breakrun565@gmail.com>
2023-05-14 12:22:20 -0400

    Æ
    æ

--- js/layers.js
+++ js/layers.js
@@ -22,7 +22,37 @@ addLayer("1", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "1", description: "1: Reset for one points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+
+})
+addLayer("2", {
+    name: "Twos", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "2", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#533CA2",
+    requires: new Decimal(100), // Can be a function that takes requirement increases into account
+    resource: "two points", // Name of prestige currency
+    baseResource: "one points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "2", description: "2: Reset for two points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+
 })
