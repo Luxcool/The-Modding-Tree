e50cb57c3ed931fd84dc182a78cfea8ce108cdd2
Luxcool <Breakrun565@gmail.com>
2024-03-04 22:50:03 -0500

    0.0.1.4 for real now
    2 is actually gone, and 1 should be seen now.

--- js/layers.js
+++ js/layers.js
@@ -34,37 +34,5 @@ addLayer("1", {
             cost: new Decimal(10),
         },
     },
-    layerShown(){return true}
-
-})
-addLayer("2", {
-    name: "twos", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "2", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-    startData() { return {
-        unlocked: true,
-		points: new Decimal(10),
-    }},
-    color: "#533CA2",
-    requires: new Decimal(100, ones), // Can be a function that takes requirement increases into account
-    resource: "twos", // Name of prestige currency
-    baseResource: "ones", // Name of resource prestige is based on
-    baseAmount() {return player.ones}, // Get the current amount of baseResource
-    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: (x/10)^0.5, // Prestige currency exponent
-    gainMult() { // Calculate the multiplier for main currency from bonuses
-        mult = new Decimal(1)
-        return mult
-    },
-    gainExp() { // Calculate the exponent on main currency from bonuses
-        return new Decimal(1)
-    },
-    row: 1, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "2", description: "2: Reset ones for twos", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
     layerShown(){return true},
-
-   
-
 })
